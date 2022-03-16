9f79fee1500e1191daba09ae98f3743faeba03e2
EloualiInes <98994070+EloualiInes@users.noreply.github.com>
2022-03-16 21:50:21 +0100

    Amélioration de l'aspect design + et jeu mise en place de l'iniatialisation du jeu

--- public/main.js
+++ public/main.js
@@ -1,32 +1,82 @@
 // Element du dom
 let inputJ1 = document.querySelector("#j1");
 let inputJ2 = document.querySelector("#j2");
+let inputs = [inputJ1, inputJ2];
 let formulaire = document.querySelector("#formulaireNoms");
 let containerFormulaire = document.querySelector(".containerFormulaire");
 let container_jeu = document.querySelector(".container_jeu");
 let table = document.querySelector(".container_jeu");
+let cases = document.querySelectorAll("td");
+let EtatJeu = [];
 
 // Etat par défaut 
 container_jeu.style.display = "none";
 
 // Variables
-let nomJ1, nomJ2;
 
+let tour;
+let joueur1 = {
+    nom: "",
+    pion:"croix",
+    adressePion : "./public/pictures/croix.png"
+};
+
+let joueur2 = {
+    nom: "",
+    pion:"rond",
+    adressePion : "./public/pictures/rond.png"
+};
 
 // Fonctions
 
 function playerStart(){
-    return (Math.floor(Math.random()*2) == 0?nomJ1:nomJ2);
+    return (Math.floor(Math.random()*2) == 0?joueur1:joueur2);
+}
+
+function chgtour(){
+
+    if (tour === joueur1) {
+        tour = joueur2
+    } else {
+        tour = joueur1;
+    }
+    let p = document.querySelector(".instructions");
+    p.innerHTML = "Au tour de <strong>" + tour.nom + "</strong>";
 }
 
 function styleInputEmpty(input, color) {
     input.style.borderColor = color;
 }
+// function verifWin(pos){
+//     let alignement = 0;
+//     if(EtatJeu[pos] == EtatJeu[pos - 3] && EtatJeu[pos] == EtatJeu[pos + 3]){
 
-
+//     }
+//     return false;
+// }
+// function verifEndPartie(){
+//     let cpt = 0;
+//     for(let i = 0; i < cases.length;i++){
+//         if(cases[i].innerHTML == ""){
+//             cpt++;
+//         }
+        
+//     }
+//     console.log("il y a " + cpt + "cases vides");
+// }
 
 
 // Event
+for(let i = 0; i < cases.length; i++){
+    cases[i].addEventListener("click",() => {
+        cases[i].innerHTML = "<img src=\" " + tour.adressePion + "\">";
+        EtatJeu[i] = tour;
+        // verifier si le symbole est répété plusieurs fois
+        // verifWin(i);
+        chgtour();
+        // verifEndPartie();
+    });
+}
 
 formulaire.addEventListener("submit", (event) =>{
     event.preventDefault();
@@ -45,11 +95,14 @@ formulaire.addEventListener("submit", (event) =>{
     else{
         styleInputEmpty(inputJ1,"silver");
         styleInputEmpty(inputJ2,"silver");
-        nomJ1 = inputJ1.value;
-        nomJ2 = inputJ2.value;
+        joueur1.nom = inputJ1.value;
+        joueur2.nom = inputJ2.value;
         containerFormulaire.style.display = "none";
         let p = document.createElement("p");
-        p.innerText = "Le player " + playerStart() + " commence";
+        p.classList.add("instructions");
+        tour = playerStart();
+        console.log(joueur1);
+        p.innerHTML = "<strong>" + tour.nom + "</strong> commence...";
         container_jeu.prepend(p);
         container_jeu.style.display = "flex";
     }
