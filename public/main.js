24ac2907a597b0ab7c83b784208f98484dcb4946
EloualiInes <98994070+EloualiInes@users.noreply.github.com>
2022-03-17 13:53:13 +0100

    Detection quand la partie est win avec des pions déposés à l'horizontal + détection quand le morpion est rempli

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ let containerFormulaire = document.querySelector(".containerFormulaire");
 let container_jeu = document.querySelector(".container_jeu");
 let table = document.querySelector(".container_jeu");
 let cases = document.querySelectorAll("td");
-let EtatJeu = [];
+
 
 // Etat par défaut 
 container_jeu.style.display = "none";
@@ -26,6 +26,8 @@ let joueur2 = {
     pion:"rond",
     adressePion : "./public/pictures/rond.png"
 };
+let LENGTHTAB = 3;
+let EtatJeu = new Array(LENGTHTAB);
 
 // Fonctions
 
@@ -47,34 +49,72 @@ function chgtour(){
 function styleInputEmpty(input, color) {
     input.style.borderColor = color;
 }
-// function verifWin(pos){
-//     let alignement = 0;
-//     if(EtatJeu[pos] == EtatJeu[pos - 3] && EtatJeu[pos] == EtatJeu[pos + 3]){
 
-//     }
-//     return false;
-// }
-// function verifEndPartie(){
-//     let cpt = 0;
-//     for(let i = 0; i < cases.length;i++){
-//         if(cases[i].innerHTML == ""){
-//             cpt++;
-//         }
-        
-//     }
-//     console.log("il y a " + cpt + "cases vides");
-// }
+// Etat Jeu
+function initEtatJeu(){
+    for(let i = 0; i < EtatJeu.length; i++){
+        EtatJeu[i] = new Array(3);
+    }
+}
 
+function ajoutEtatJeu(pos, obj){
+    
+    let x = (pos!= 0)?~~(pos/LENGTHTAB):0;
+    let y = (pos!= 0)?pos%LENGTHTAB:0;
+    EtatJeu[x][y] = obj;
+    if(verifWin(x,y) || morpionRempli()){
+        console.log("gagné or morpion rempli");
+        // OUVERTURE pop pour le gain
+    }
+    
+}
 
+// Verifie l'alignement de trois symboles à l'horizotal
+function horizontal(x,y){
+    let newY = y;
+    let alignement = 1;
+    for(let i = 1; i < LENGTHTAB; i++){
+        // Pion posé au centre de la ligne
+        if(y != 1){
+            newY = (y + i < LENGTHTAB)?y+i:y-i;
+        }
+        // Pion posé au extrémités
+        else{
+            newY = (newY + i < LENGTHTAB)?newY+i:newY-i;
+        }
+        // Si le pion posé et le pion à coté sont egaux incrémente l'alignement
+        if(EtatJeu[x][y] == EtatJeu[x][newY]){
+            alignement++;
+        }
+        else{
+            break;
+        }
+    }
+    return alignement;
+}
+function verifWin(x,y){
+    if(horizontal(x,y) == 3){
+        return true;
+    }
+    return false;
+}
+
+function morpionRempli(){
+    for(let x = 0; x < EtatJeu.length; x++){
+        for(let y = 0; y < EtatJeu.length; y++){
+            if(EtatJeu[x][y] == null)
+                return false;
+        }
+    }
+    return true;
+}
 // Event
 for(let i = 0; i < cases.length; i++){
     cases[i].addEventListener("click",() => {
         cases[i].innerHTML = "<img src=\" " + tour.adressePion + "\">";
-        EtatJeu[i] = tour;
-        // verifier si le symbole est répété plusieurs fois
-        // verifWin(i);
+        ajoutEtatJeu(i,tour);
+
         chgtour();
-        // verifEndPartie();
     });
 }
 
@@ -100,6 +140,7 @@ formulaire.addEventListener("submit", (event) =>{
         containerFormulaire.style.display = "none";
         let p = document.createElement("p");
         p.classList.add("instructions");
+        initEtatJeu();
         tour = playerStart();
         console.log(joueur1);
         p.innerHTML = "<strong>" + tour.nom + "</strong> commence...";
